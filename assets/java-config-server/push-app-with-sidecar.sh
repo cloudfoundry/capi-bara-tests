31b02b3d3e8c3ae5146510b87d1956418895267b
Tim Downey <tdowney@pivotal.io>
2019-04-30 14:18:47 -0700

    Introduce java-dora and java-config-server sidecar
    - Created java-config-server/push-app-with-sidecar.sh
      put the sidecar jar inside the main app jar so the
      java-buildpack can place them both inside the same
      app container.
    
    - Added a bespoke sidecar command inside dora_manifest.yml
      in order to start up the java-config-server sidecar.
    
    [#165404674]
    
    Co-authored-by: Tim Downey <tdowney@pivotal.io>
    Co-authored-by: Andy Brown <anbrown@pivotal.io>
    Co-authored-by: Mona Mohebbi <mmohebbi@pivotal.io>
    Co-authored-by: Sannidhi Jalukar <sjalukar@pivotal.io>

---
+++ assets/java-config-server/push-app-with-sidecar.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -ex
+
+DORA_PATH="../java-dora"
+DORA_MANIFEST_PATH="../java-config-server/dora_manifest.yml"
+JAVA_DORA_JAR="build/libs/java-dora-0.0.1-SNAPSHOT.jar"
+JAVA_CONFIG_JAR="build/libs/java-config-server-0.0.1-SNAPSHOT.jar"
+
+function clean() {
+    ./gradlew clean
+}
+
+function build_config_server() {
+    ./gradlew build
+    cp build/libs/*.jar "${DORA_PATH}/build/libs"
+}
+
+function push_dora_with_sidecars() {
+    pushd ${DORA_PATH} > /dev/null
+        ./gradlew build
+        zip "${JAVA_DORA_JAR}" -u "${JAVA_CONFIG_JAR}"
+        cf v3-create-app java-dora
+        cf v3-apply-manifest -f "${DORA_MANIFEST_PATH}"
+        cf v3-push java-dora -p build/libs/java-dora-0.0.1-SNAPSHOT.jar
+    popd > /dev/null
+}
+
+function main() {
+    clean
+    build_config_server
+    push_dora_with_sidecars
+}
+
+main
