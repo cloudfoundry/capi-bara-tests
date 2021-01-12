d3601532220b1ee40ba76ec7015bf44239215315
Carson Long <lcarson@vmware.com>
2021-01-12 22:10:27 +0000

    test updating healthcheck http enpoint with ZDT
    [finishes #175277551]
    
    Co-authored-by: Carson Long <lcarson@vmware.com>
    Co-authored-by: Merric de Launey <mdelauney@pivotal.io>

--- baras/zero_downtime.go
+++ baras/zero_downtime.go
@@ -58,6 +58,26 @@ var _ = Describe("Zero downtime operations", func() {
 		})
 	})
 
+	Context("When changing the healthcheck http endpoint on an app", func() {
+		It("downtime does not occur until the app is restarted", func() {
+			originalUptime, err := strconv.ParseFloat(helpers.CurlApp(Config, appName, "/uptime"), 64)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(originalUptime).To(BeNumerically(">", 0))
+
+			process := GetFirstProcessByType(GetProcesses(appGUID, appName), "web")
+			path := fmt.Sprintf("v3/processes/%s", process.Guid)
+			session := cf.Cf("curl", "-X", "PATCH", path, "-d", `{"health_check": {"data": {"endpoint": "/two"}}}`).Wait()
+			Eventually(session).Should(Exit(0))
+			result := session.Out.Contents()
+			Expect(strings.Contains(string(result), "errors")).To(BeFalse())
+			// Allow time for diego clock sync to occur (default=30s)
+			time.Sleep(35 * time.Second)
+			currentUptime, err := strconv.ParseFloat(helpers.CurlApp(Config, appName, "/uptime"), 64)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(currentUptime).To(BeNumerically(">", originalUptime+33))
+		})
+	})
+
 	Context("When changing the healthcheck type on an app", func() {
 		It("downtime does not occur until the app is restarted", func() {
 			originalUptime, err := strconv.ParseFloat(helpers.CurlApp(Config, appName, "/uptime"), 64)
